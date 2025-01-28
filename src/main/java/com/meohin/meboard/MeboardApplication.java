bdc7eb0ea721fba39d8f299ceabcd98726549331
meohin <meohin.meo@gmail.com>
2025-01-28 12:07:22 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/meohin/meboard/MeboardApplication.java
@@ -0,0 +1,13 @@
+package com.meohin.meboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meohin/meboard/MeboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meohin.meboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
