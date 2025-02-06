9c84ae0efadd40224a908d8635991b0d2e03259d
meohin <meohin.meo@gmail.com>
2025-02-06 15:04:24 +0900

    feat: 댓글 조회 쿼리 추가

--- src/main/java/com/meohin/meboard/repository/ReplyRepository.java
+++ src/main/java/com/meohin/meboard/repository/ReplyRepository.java
@@ -1,8 +1,12 @@
 package com.meohin.meboard.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.meohin.meboard.entity.Reply;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    List<Reply> findByPostId(Long postId);
 }
