b2e8d1d26347d2ce7873f822af112dd2b85a7f34
meohin <meohin.meo@gmail.com>
2025-02-11 18:04:36 +0900

    feat: 대댓글 기능을 위한 Reply 엔티티 수정
    - Reply 엔티티에 부모 댓글(parent)와 자식 댓글(children) 관계 추가
    - @OneToMany 및 @ManyToOne을 활용한 계층 구조 설정
    - FetchType.LAZY 적용으로 성능 최적화
    - 기존 필드 유지 및 대댓글 관리를 위한 parent_id 컬럼 추가

--- src/main/java/com/meohin/meboard/entity/Reply.java
+++ src/main/java/com/meohin/meboard/entity/Reply.java
@@ -1,14 +1,19 @@
 package com.meohin.meboard.entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -42,4 +47,11 @@ public class Reply {
     @ManyToOne
     @JoinColumn(name = "post_id")
     private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Reply parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private List<Reply> children = new ArrayList<>();
 }
