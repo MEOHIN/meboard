2d0a98ec3e29c61ec5d5f37036796a7a619ce75e
meohin <meohin.meo@gmail.com>
2025-02-12 18:14:06 +0900

    feat: Spring Security 기본 설정 추가
    - 모든 요청을 허용하도록 설정

---
+++ src/main/java/com/meohin/meboard/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.meohin.meboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+            .authorizeHttpRequests(
+                (authorizeHttpRequests) -> 
+                    authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**"))
+                        .permitAll()
+            );
+        
+        return httpSecurity.build();
+    }
+}
