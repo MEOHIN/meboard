ef2297653477b754b531043f11710e808e2c6203
meohin <meohin.meo@gmail.com>
2025-02-12 19:10:48 +0900

    feat: UserController 추가
    - 회원가입, 로그인, 로그아웃, 회원정보, 프로필 수정, 회원정보 수정, 회원탈퇴, 좋아요한 게시글 목록, 작성한 댓글 목록 기능 추가

---
+++ src/main/java/com/meohin/meboard/controller/UserController.java
@@ -0,0 +1,85 @@
+package com.meohin.meboard.controller;
+
+import org.glassfish.jaxb.core.v2.TODO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    
+    // 회원가입
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(Model model) {
+        return "redirect:/post/list";
+    }
+
+    // 로그인
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    // 로그아웃
+    @PostMapping("/logout")
+    public String logout() {
+        return "redirect:/post/list";
+    }
+    
+    // 회원정보
+    @GetMapping("/mypage")
+    public String mypage() {
+        return "mypage";
+    }
+
+    // 프로필
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile";
+    }
+
+    @PostMapping("/profile")
+    public String profile(Model model) {
+        return "redirect:/user/profile";
+    }
+    
+    // 회원정보 수정
+    @GetMapping("/modify")
+    public String modify() {
+        return "modify";
+    }
+
+    @PostMapping("/modify")
+    public String modify(Model model) {
+        return "redirect:/user/modify";
+    }
+
+    // 회원탈퇴
+    @PostMapping("/delete")
+    public String delete() {
+        return "redirect:/";
+    }
+
+    // 좋아요한 게시글 목록
+    @GetMapping("/like")
+    public String like() {
+        return "user/like";
+    }
+
+    // 작성한 댓글 목록
+    @GetMapping("/reply")
+    public String reply() {
+        return "user/reply";
+    }
+}
