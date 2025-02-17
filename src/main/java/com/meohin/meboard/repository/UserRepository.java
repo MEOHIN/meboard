c499c9ad9fccc7f58a74f6d59a69648aef1dafc8
meohin <meohin.meo@gmail.com>
2025-02-17 13:43:33 +0900

    feat: 사용자 레포지토리(UserRepository) 수정
    - `existsByUsername`: 아이디(이메일) 중복 여부 확인 메서드 추가
    - `existsByNickname`: 닉네임 중복 여부 확인 메서드 추가
    - `findByUsername`: 아이디로 사용자 조회 메서드 추가

--- src/main/java/com/meohin/meboard/repository/UserRepository.java
+++ src/main/java/com/meohin/meboard/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.meohin.meboard.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.meohin.meboard.entity.SiteUser;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    boolean existsByUsername(String username);
+    boolean existsByNickname(String nickname);
+    Optional<SiteUser> findByUsername(String username);
 }
