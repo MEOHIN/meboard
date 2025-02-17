278c0bf87fbe40c2d41ad42ab0a133ae6e115d2c
meohin <meohin.meo@gmail.com>
2025-02-17 13:48:00 +0900

    feat: 로그인, 로그아웃 및 패스워드 인코더 설정 추가
    - 로그인 페이지 및 로그인 성공 후 리다이렉트 URL 설정
    - 로그아웃 기능 구현 (로그아웃 후 홈으로 리다이렉트 및 세션 무효화)
    - `PasswordEncoder`를 BCrypt로 설정하여 비밀번호 암호화
    - 모든 요청에 대해 기본적인 접근 권한 허용 설정

--- src/main/java/com/meohin/meboard/config/SecurityConfig.java
+++ src/main/java/com/meohin/meboard/config/SecurityConfig.java
@@ -4,9 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -19,7 +24,16 @@ public class SecurityConfig {
                         .requestMatchers(new AntPathRequestMatcher("/**"))
                         .permitAll()
             );
+
+        httpSecurity.formLogin((formLogin) -> formLogin.loginPage("/user/login").defaultSuccessUrl("/"));
+        httpSecurity.logout((logout) -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/user/logout")).logoutSuccessUrl("/").invalidateHttpSession(true));
         
         return httpSecurity.build();
+    }
+
+    // PasswordEncoder를 빈(bean)으로 등록
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
