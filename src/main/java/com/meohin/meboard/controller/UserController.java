c3e0262a70afd5b5a11fda637f1bf382f222f700
meohin <meohin.meo@gmail.com>
2025-02-17 13:35:29 +0900

    feat: 회원가입 로직 개선 및 검증 기능 추가
    - `UserVO`를 사용하여 회원가입 폼 데이터 검증 추가
    - `BindingResult`를 활용한 입력값 검증 로직 추가
    - 아이디 및 닉네임 중복 검사 기능 추가
    - 비밀번호 일치 여부 검증 추가
    - 회원가입 성공 시 로그인 페이지로 리디렉트

--- src/main/java/com/meohin/meboard/controller/UserController.java
+++ src/main/java/com/meohin/meboard/controller/UserController.java
@@ -1,26 +1,73 @@
 package com.meohin.meboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.meohin.meboard.service.UserService;
+import com.meohin.meboard.vo.UserVO;
+
 import ch.qos.logback.core.model.Model;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
     
     // 회원가입
+    // /user/signup URL 이 GET 으로 요청되면 회원 가입을 위한 validation object인 UserVO를 렌더링
     @GetMapping("/register")
-    public String register() {
+    public String register(UserVO userVO) {
         return "register";
     }
 
     @PostMapping("/register")
-    public String register(Model model) {
-        return "redirect:/post/list";
+    public String register(@Valid UserVO userVO, BindingResult bindingResult) {
+        // 1. 입력값 검증
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+
+        // 2. 아이디 중복 검사
+        try {
+            if (userService.isUsernameTaken(userVO.getUsername())) {
+                bindingResult.rejectValue("username", "duplicateUsername", "이미 등록된 아이디입니다.");
+                return "register";
+            }
+        } catch (Exception e) {
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "register";
+        }
+
+        // 3. 닉네임 중복 검사
+        if (userService.isNicknameTaken(userVO.getNickname())) {
+            bindingResult.rejectValue("nickname", "duplicateNickname", "이미 등록된 닉네임입니다.");
+            return "register";
+        }
+
+        // 4. 비밀번호 일치여부 검사
+        if (!userVO.getPassword1().equals(userVO.getPassword2())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect", "비밀번호와 비밀번호 확인이 다릅니다.");
+            return "register";
+        }
+
+        // 5. 회원가입 진행
+        try {
+            userService.register(userVO.getUsername(), userVO.getNickname(), userVO.getPassword1());
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "register";
+        }
+
+        return "redirect:/user/login";
     }
 
     // 로그인
