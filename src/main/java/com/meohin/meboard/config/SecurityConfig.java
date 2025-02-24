c200297a83c52a65141bd56f4eccc68d1b9516c5
meohin <meohin.meo@gmail.com>
2025-02-24 15:55:11 +0900

    remove: 불필요한 코드 삭제

--- src/main/java/com/meohin/meboard/config/SecurityConfig.java
+++ src/main/java/com/meohin/meboard/config/SecurityConfig.java
@@ -7,9 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
