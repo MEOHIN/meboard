a0b4fc938921422628c03234f54b9fd96e1a031e
meohin <meohin.meo@gmail.com>
2025-02-26 19:00:34 +0900

    remove: 불필요한 import문 삭제

--- src/main/java/com/meohin/meboard/controller/UserController.java
+++ src/main/java/com/meohin/meboard/controller/UserController.java
@@ -3,8 +3,6 @@ package com.meohin.meboard.controller;
 import java.security.Principal;
 import java.util.List;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
